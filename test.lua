a604e57bf872b9659d5e58c424791aa70a0f6f68
ZhenZzJ <sharkkapainwza007@gmail.com>
2026-03-26 11:59:19 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,7 @@
-
+-- Full Bright
+game.Lighting.Brightness = 2
+game.Lighting.ClockTime = 14
+game.Lighting.FogEnd = 100000
+game.Lighting.GlobalShadows = false
+game.Lighting.Ambient = Color3.new(1, 1, 1)
+game.Lighting.OutdoorAmbient = Color3.new(1, 1, 1)
